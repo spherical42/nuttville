69b087e161e22ee7a70b37413b13d4d36b8f6025
spherical42 <ayerstjamesbb8@gmail.com>
2023-06-26 22:32:51 -0400

    better cooldowns

--- source/playerstuff/characters/Carlos.gd
+++ source/playerstuff/characters/Carlos.gd
@@ -6,6 +6,7 @@ func _ready():
 	selectid = 1
 	maxhp = 800
 	playerspeed = 5
+	maxcds = [2,2,6,10] #[lclick, rclick, shift, space] seconds of cooldown
 	._ready()
 
 func _get_custom_rpc_methods():
@@ -20,12 +21,12 @@ func _get_custom_rpc_methods():
 func DoAttacks(p, c):
 	if p[0] == 1 && c[0] == 0: # Left click
 		print("lclick")
-		cooldowns[0] = 2
+		cooldowns[0] = maxcds[0]
 		pass
 	
 	if p[1] == 1 && c[1] == 0: # Right click
 		print("rclick")
-		cooldowns[1] = 2
+		cooldowns[1] = maxcds[1]
 		pass
 	
 	if p[2] == 1: # Shift
--- source/playerstuff/playerClass.gd
+++ source/playerstuff/playerClass.gd
@@ -14,7 +14,8 @@ var last_dir : Vector2
 var zoom = 1
 var playerspeed = 7
 var pressed = [0,0,0,0,0] #[lclick, rclick, shift, ult, space] 1 for pressed 0 for not
-var cooldowns = [0,0,0,0,0] #[lclick, rclick, shift, ult, space] frames of cooldown left
+var cooldowns = [0,0,0,0,0] #[lclick, rclick, shift, ult, space] seconds of cooldown left
+var maxcds = [0,0,0,0,0] #in seconds
 var maxhp = 1000
 var hp = 1000
 var oldpos
